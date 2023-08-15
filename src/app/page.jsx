fb4377d7e5ec918363424f9d070638a8d58cdc1d
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-08-15 17:32:26 -0300

    fazenda

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="">
       <div className="w-screen flex justify-start items-center ">
         <h1 className="text-primary ml-32 mt-32 font-bold text-5xl w-[500px]">
-          Queijos Artesanais de Alagoa: Leve para Casa o Verdadeiro Sabor das Montanhas!
+          Queijos Artesanais de Alagoa. Leve para Casa o Verdadeiro Sabor das Montanhas!
         </h1>
         <Image src={queijo} alt="queijo" className="absolute top-44 -z-10"/>
       </div>
